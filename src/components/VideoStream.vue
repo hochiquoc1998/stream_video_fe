aa9545a46b7128018006c281b9972335ee4aea5b
quoc.hc <hochiquoc1998>
2024-07-15 12:05:53 +0700

    up

--- src/components/VideoStream.vue
+++ src/components/VideoStream.vue
@@ -10,9 +10,24 @@ export default {
   methods: {
     async startCamera() {
       try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: true,
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        let videoSource = null;
+
+        devices.forEach((device) => {
+          if (device.kind === 'videoinput') {
+            if (device.label.toLowerCase().includes('back')) {
+              videoSource = device.deviceId;
+            }
+          }
         });
+
+        const constraints = {
+          video: {
+            deviceId: videoSource ? { exact: videoSource } : undefined,
+          },
+        };
+
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
         this.$refs.video.srcObject = stream;
       } catch (error) {
         console.error('Error accessing the camera: ', error);
