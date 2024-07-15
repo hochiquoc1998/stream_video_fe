b4d28864b7bdfe3ccbc19089874bf1d429bb97eb
quoc.hc <hochiquoc1998>
2024-07-15 12:08:40 +0700

    up

--- src/components/VideoStream.vue
+++ src/components/VideoStream.vue
@@ -11,19 +11,21 @@ export default {
     async startCamera() {
       try {
         const devices = await navigator.mediaDevices.enumerateDevices();
-        let videoSource = null;
+        let backCamera = null;
 
         devices.forEach((device) => {
-          if (device.kind === 'videoinput') {
-            if (device.label.toLowerCase().includes('back')) {
-              videoSource = device.deviceId;
-            }
+          if (
+            device.kind === 'videoinput' &&
+            device.label.toLowerCase().includes('back')
+          ) {
+            backCamera = device.deviceId;
           }
         });
 
         const constraints = {
           video: {
-            deviceId: videoSource ? { exact: videoSource } : undefined,
+            deviceId: backCamera ? { exact: backCamera } : undefined,
+            facingMode: backCamera ? undefined : { exact: 'environment' },
           },
         };
 
